4f4a9c73f9cb47f57ee2a0c09d4dffecb4a3ba4f
rbs392 <rbs392@yahoo.com>
2017-12-21 01:43:21 +0530

    fixing mpids

--- scripts/static/main.js
+++ scripts/static/main.js
@@ -7,7 +7,6 @@ function populateProducts (products) {
   $("#products").empty();
   products.forEach(function (product) {
     regex.lastIndex = 0
-    console.log()
     $("#products").append('<div class="col-sm-12 p-card">'+
     '<div class="row">'+
       '<div class="thumbnail col-sm-4 text-center">'+
@@ -65,7 +64,7 @@ function populateProductsByType (type, products, responseTime) {
           '<div class="tags float-left" style="font-family: Arial; font-size: 11px;">'+product.brandName+'</div>'+
           '<div class="tags float-left" style="font-family: Arial; font-size: 11px;">searchScore: '+product.searchScore+'</div>'+
           // '<div class="tags float-left" style="font-family: Arial; font-size: 11px;">$'+ product.priceRange[0].salePrice+' - '+ product.priceRange[1].salePrice +'</div>'+
-          '<div class="tags float-left" style="font-family: Arial; font-size: 11px;">mpid: '+(product.mpidStr || ((mpid != null) ? mpid[1] : ""))+'</div>'+
+          '<div class="tags float-left" style="font-family: Arial; font-size: 11px;">mpid: '+(product.mpidStr || product.mpid || mpid[1])+'</div>'+
           '<div class="tags float-left" style="font-family: Arial; font-size: 11px;">sku: '+ (product.sku ? product.sku : Object.keys(product.stores || {})[0] ? (((product.stores[Object.keys(product.stores || {})[0]] || {}).offers || [])[0] || {}).sku : undefined) +'</div>'+
         '</div>'+
       '</div>'+
